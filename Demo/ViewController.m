42831788c1086e67087a081216e03710e640ef7f
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-08-15 08:51:45 +0200

    Add demo reporting

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -23,8 +23,24 @@
     [Seeds sharedInstance].inAppMessageDelegate = self;
 }
 
+- (void)seedsInAppMessageClicked:(SeedsInAppMessage*)inAppMessage {
+    NSLog(@"seedsInAppMessageClicked");
+}
+
+- (void)seedsInAppMessageClosed:(SeedsInAppMessage*)inAppMessage andCompleted:(BOOL)completed {
+    NSLog(@"seedsInAppMessageClosed, completed = %@", completed ? @"YES" : @"NO");
+}
+
 - (void)seedsInAppMessageLoadSucceeded:(SeedsInAppMessage*)inAppMessage {
     [[Seeds sharedInstance] showInAppMessageIn:self];
+}
+
+- (void)seedsInAppMessageShown:(SeedsInAppMessage*)inAppMessage withSuccess:(BOOL)success {
+    NSLog(@"seedsInAppMessageShown, success = %@", success ? @"YES" : @"NO");
+}
+
+- (void)seedsNoInAppMessageFound {
+    NSLog(@"seedsNoInAppMessageFound");
 }
 
 - (IBAction)iapEvent:(id)sender {
