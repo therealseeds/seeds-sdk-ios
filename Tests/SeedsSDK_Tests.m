22bae47f8c418a620497ac777cd08748fedb4939
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-09-14 14:20:44 +0300

    Amend tests

--- Tests/SeedsSDK_Tests.m
+++ Tests/SeedsSDK_Tests.m
@@ -67,12 +67,6 @@
 
 #pragma mark - SeedsInAppMessageDelegate
 
-- (void)seedsInAppMessageShown:(SeedsInAppMessage*)inAppMessage withSuccess:(BOOL)success {
-
-    _seedsInAppMessageShown = success;
-
-}
-
 - (void)seedsInAppMessageLoadSucceeded:(SeedsInAppMessage*)inAppMessage {
 
     _seedsInAppMessageLoaded = YES;
@@ -80,21 +74,8 @@
 
 }
 
-
 - (void)seedsNoInAppMessageFound {
     _seedsNotFound = YES;
 }
-
-
-
-
-
-
-//- (void)testPerformanceExample {
-//    // This is an example of a performance test case.
-//    [self measureBlock:^{
-//        // Put the code you want to measure the time of here.
-//    }];
-//}
 
 @end
